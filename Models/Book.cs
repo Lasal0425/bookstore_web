b792913ce6c3ef92abb6892e0a730f521e63e4e0
Lasal0425 <lasalrathnayake@gmail.com>
2025-05-21 20:35:47 +0530

    Changes in the book class

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,13 +1,11 @@
-using System;
-
 namespace BookApi.Models
 {
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string ISBN { get; set; }
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+        public required string ISBN { get; set; }
         public DateTime PublicationDate { get; set; }
     }
 }
